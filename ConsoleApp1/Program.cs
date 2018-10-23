81f8e9569c3ed0a0b08d36f4d26c89a449018e85
Synless <Synless@users.noreply.github.com>
2018-10-23 15:34:10 +0200

    Issue #5 - 5th try - Corner/F.lux/Timings
    Corners correction
    Added F.lux behavior
    Added idle timing bounded to the pace of the images

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,6 +6,7 @@ using System.Windows;
 using System;
 using System.Net.Sockets;
 using System.Drawing.Drawing2D;
+using System.Diagnostics;
 
 namespace ConsoleApp1
 {
@@ -17,26 +18,31 @@ namespace ConsoleApp1
         const int Shift = 0; //change to match 21/9 content on 16/9 screen
         const int UpDown = 0; //change if first LED is not in the corner
         const bool Clockwise = false; //Clockwise of counter-clockwise
-        const int Corner = (int)corner.bottomRight; //Position of the first LED
+        const int Corner = 0; //Number of LEDs that should be needed to complete the strip to the very corner
+        const int StartCorner = (int)corner.bottomRight; //Position of the first LED
         static readonly EndPoint ESP = new IPEndPoint(new IPAddress(new byte[4] { 192, 168, 0, 30 }), 8787); //ESP IP and port 8787
         const bool LowPassFilter = true; //True = apply low-pass filter, false = does not apply low-pass filter
+        const bool FluxFilter = true;
+        const bool useDynamicTiming = true; //False : will use fixed timing defined by SleepingTime
+        const int minIdleValue = 2;
+        const int maxIdleValue = 150;
         const int SleepingTime = 5; //milli-seconds idle time, reduces CPU usage and frequency
         #endregion
 
         #region Variables not to touch
         static Socket sock = new Socket(SocketType.Dgram, ProtocolType.Udp);
-        static Rectangle edgeLeft;
+        /*static Rectangle edgeLeft;
         static Rectangle edgeRight;
         static Rectangle edgeTop;
         static Rectangle edgeBot;
-        static Bitmap bmpScreenshot;
+        static Bitmap bmpScreenshot;*/
         static Bitmap scaledBmpScreenshot;
-        static Bitmap secondScaledBmpScreenshot;
+        //static Bitmap secondScaledBmpScreenshot;
         static Bitmap scalededgeLeft;
         static Bitmap scalededgeRight;
         static Bitmap scalededgeTop;
         static Bitmap scalededgeBot;
-        static bool screenConfigured = false;
+        /*static bool screenConfigured = false;
         static bool debug = false;
         static int startX;
         static int startY;
@@ -45,14 +51,28 @@ namespace ConsoleApp1
         static int hX;
         static int hY;
         static Rectangle rect;
-        static Bitmap bmp;
+        static Bitmap bmp;*/
 
-        static double sRed = 255;
+        /*static double sRed = 255;
         static double sGreen = 255;
-        static double sBlue = 255;
+        static double sBlue = 255;*/
         static List<byte> LastByteToSend = new List<byte>(0);
         static List<byte> newByteToSend = new List<byte>(0);
         static List<byte> byteToSend;
+
+        static DateTime startTime = DateTime.Parse("00:00:00");
+        static DateTime midDay = DateTime.Parse("12:00:00");
+        static DateTime endTime = DateTime.Parse("05:00:00");
+        const double nbMinutesStart = 200.0;
+        const double nmMinutesStop = 250.0;
+        static int startTimeInt;
+        static int endTimeInt;
+        static int currentTimeInt;
+
+        static double fluxRatio = 1.0;
+
+        static int difference = 0;
+        static int currentSleepTime = 5;       
 
         enum corner
         {
@@ -81,9 +101,9 @@ namespace ConsoleApp1
 
             while (true)
             {
-                Console.WriteLine("- Tick - " + tickCount++);
+                Console.WriteLine("- Tick - " + tickCount++ + "\t currentSleepTime = " + currentSleepTime);
                 Tick();
-                System.Threading.Thread.Sleep(SleepingTime);
+                System.Threading.Thread.Sleep(currentSleepTime);
             }
         }
         static void Tick()
@@ -94,7 +114,6 @@ namespace ConsoleApp1
         }
         static void GetScreenShot()
         {
-            Rectangle screenToCapture;
             int xScreen = (int)(SystemParameters.PrimaryScreenWidth);
             int yScreen = (int)(SystemParameters.PrimaryScreenHeight);
 
@@ -140,7 +159,6 @@ namespace ConsoleApp1
             {
                 if (false)
                 {
-                    debug = false;
                     ResizeSizes(scalededgeLeft).Save("1Left.bmp");
                     ResizeSizes(scalededgeRight).Save("3Right.bmp");
                     ResizeTops(scalededgeTop).Save("2Top.bmp");
@@ -151,7 +169,6 @@ namespace ConsoleApp1
             catch
             {
             }
-
         }
         static Bitmap Resize(Bitmap srcImage)
         {
@@ -197,7 +214,7 @@ namespace ConsoleApp1
                 int subCorner = Math.Max(0, Corner - 1);
                 if (Clockwise)
                 {
-                    if (Corner==corner.topLeft)
+                    if (StartCorner == corner.topLeft)
                     {
                         for (int x = Corner; x < scaledBmpScreenshot.Width - 1 - Corner; x++)
                         {
@@ -224,7 +241,7 @@ namespace ConsoleApp1
                             byteToSend.Add((scaledBmpScreenshot.GetPixel(0, Math.Max(0, Math.Min(y, scaledBmpScreenshot.Height - 1))).B));
                         }
                     }
-                    if (Corner == corner.topRight)
+                    if (StartCorner == corner.topRight)
                     {
                         for (int y = subCorner; y < scaledBmpScreenshot.Height - 1 - subCorner; y++)
                         {
@@ -251,7 +268,7 @@ namespace ConsoleApp1
                             byteToSend.Add((scaledBmpScreenshot.GetPixel(x, 0).B));
                         }
                     }
-                    if (Corner == corner.bottomRight)
+                    if (StartCorner == corner.bottomRight)
                     {
                         for (int x = scaledBmpScreenshot.Width - 1 - Corner; x > Corner; x--)
                         {
@@ -278,7 +295,7 @@ namespace ConsoleApp1
                             byteToSend.Add((scaledBmpScreenshot.GetPixel(scaledBmpScreenshot.Width - 1, Math.Max(0, Math.Min(y, scaledBmpScreenshot.Height - 1))).B));
                         }
                     }
-                    if (Corner == corner.bottomLeft)
+                    if (StartCorner == corner.bottomLeft)
                     {
                         for (int y = scaledBmpScreenshot.Height - 1 - subCorner; y > subCorner; y--)
                         {
@@ -308,7 +325,7 @@ namespace ConsoleApp1
                 }
                 else
                 {
-                    if (Corner == corner.topLeft)
+                    if (StartCorner == corner.topLeft)
                     {
                         for (int y = subCorner; y < scaledBmpScreenshot.Height - 1 - subCorner; y++)
                         {
@@ -335,7 +352,7 @@ namespace ConsoleApp1
                             byteToSend.Add((scaledBmpScreenshot.GetPixel(x, 0).B));
                         }
                     }
-                    if (Corner == corner.topRight)
+                    if (StartCorner == corner.topRight)
                     {
                         for (int x = scaledBmpScreenshot.Width - 1 - Corner; x > Corner; x--)
                         {
@@ -362,7 +379,7 @@ namespace ConsoleApp1
                             byteToSend.Add((scaledBmpScreenshot.GetPixel(scaledBmpScreenshot.Width - 1, Math.Max(0, Math.Min(y, scaledBmpScreenshot.Height - 1))).B));
                         }
                     }
-                    if (Corner == corner.bottomRight)
+                    if (StartCorner == corner.bottomRight)
                     {
                         for (int y = scaledBmpScreenshot.Height - 1 - subCorner; y > subCorner; y--)
                         {
@@ -389,7 +406,7 @@ namespace ConsoleApp1
                             byteToSend.Add((scaledBmpScreenshot.GetPixel(x, scaledBmpScreenshot.Height - 1).B));
                         }
                     }
-                    if (Corner == corner.bottomLeft)
+                    if (StartCorner == corner.bottomLeft)
                     {
                         for (int x = Corner; x < scaledBmpScreenshot.Width - 1 - Corner; x++)
                         {
@@ -437,9 +454,58 @@ namespace ConsoleApp1
             {
                 LastByteToSend = newByteToSend = byteToSend;
             }
+
+
             if (UpDown != 0)
             {
                 RotateArray();
+            }
+
+
+            if (FluxFilter)
+            {
+                startTimeInt = startTime.Hour * 60 + startTime.Minute;
+                endTimeInt = endTime.Hour * 60 + endTime.Minute;
+                currentTimeInt = DateTime.Now.Hour * 60 + DateTime.Now.Minute;
+
+                if (currentTimeInt > startTimeInt && currentTimeInt < endTimeInt)
+                {
+                    fluxRatio = (nmMinutesStop - nbMinutesStart) / nmMinutesStop;
+                }
+                else if (Math.Abs(currentTimeInt - endTimeInt) < nbMinutesStart)
+                {
+                    fluxRatio = (nmMinutesStop - nbMinutesStart + double.Parse(Math.Abs(currentTimeInt - endTimeInt).ToString())) / nmMinutesStop;
+                }
+                else if (currentTimeInt > midDay.Hour * 60 + midDay.Minute)
+                {
+                    currentTimeInt = (24 * 60) - currentTimeInt;
+                    if (Math.Abs(currentTimeInt - startTimeInt) < nbMinutesStart)
+                    {
+                        fluxRatio = (nmMinutesStop - nbMinutesStart + double.Parse(Math.Abs(currentTimeInt - startTimeInt).ToString())) / nmMinutesStop;
+                    }
+                }
+                for (int n = 2; n < byteToSend.Count - 2; n += 3)
+                {
+                    string s;
+                    byte b;
+
+                    s = (newByteToSend[n] * fluxRatio).ToString().Split(',')[0];
+                    b = byte.Parse(s);
+                    newByteToSend[n] = b;
+
+                    s = (newByteToSend[n - 1] * ((1 + fluxRatio) / 2)).ToString().Split(',')[0];
+                    b = byte.Parse(s);
+                    newByteToSend[n - 1] = b;
+
+                    s = (newByteToSend[n - 2] * (1 + (1 - fluxRatio) / 3)).ToString().Split(',')[0];
+                    short i = short.Parse(s);
+                    if (i > 255)
+                    {
+                        i = 255;
+                    }
+                    b = byte.Parse(i.ToString());
+                    newByteToSend[n - 2] = b;
+                }
             }
 
             int packetSize = 1200;
@@ -449,7 +515,10 @@ namespace ConsoleApp1
             }
             int index = newByteToSend.Count - (newByteToSend.Count % packetSize);
             SendPayload(PayloadType.terminalPayload, newByteToSend.GetRange(index, newByteToSend.Count % packetSize));
+
+            GetIdle();
         }
+        
         static void RotateArray()
         {
             List<byte> byteToSend2 = new List<byte>(newByteToSend);
@@ -468,6 +537,28 @@ namespace ConsoleApp1
             payload.Insert(0, (byte)('A')); //magic number #1, helps eliminate the junk that is broadcasted on the network
 
             sock.SendTo(payload.ToArray(), ESP);
+        }
+
+        static void GetIdle()
+        {
+            if (newByteToSend.Count != byteToSend.Count)
+            {
+                difference = 150;
+            }
+            else
+            {
+                difference = 0;
+                for (int n = 0; n < byteToSend.Count; n++)
+                {
+                    difference += Math.Abs((int)(byteToSend[n]) - (int)(newByteToSend[n]));
+                }
+            }
+
+            double tmp = difference*5;
+            //tmp = Math.Max(0,Math.Min(5000.0,tmp));
+            tmp = (-tmp + 1000.0) / 500.0;
+            currentSleepTime += (int)tmp;
+            currentSleepTime = Math.Min(Math.Max(minIdleValue, currentSleepTime),maxIdleValue);
         }
     }
 }
